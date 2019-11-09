f22fcf13d4b87538c88f1ff010ad23b9f5529b07
dinesh chaturvedi <dotnext24@gmail.com>
2019-11-10 02:28:45 +0530

    Add project files.

---
+++ IdentityService/Config.cs
@@ -0,0 +1,35 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityService
+{
+    public class Config
+    {
+        // API that are allowed to access the Auth server
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+                   {
+                   new ApiResource("api1", "My API")
+                };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            // client credentials, list of clients
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                     {
+                       new Secret("secret".Sha256())
+                      },
+                  AllowedScopes = { "api1" }
+                }
+            };
+        }
+    }
+}
